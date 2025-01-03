7ae1d41535147672798a8b42eb68bf251667114d
Marco Dava <161601750+MarcoDava@users.noreply.github.com>
2025-01-03 16:29:49 -0500

    Update isAnagram.py

--- isAnagram.py
+++ isAnagram.py
@@ -1,17 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        #Make hashmap and compare the value amounts of each
+        #try not to sort, bad timing complexity
+        #convert to hashmap then compare length of the hashmaps
+        if len(s)!=len(t):
+            return False
+        hash_table1={}
+        hash_table2={}
 
-        return Counter(s) == Counter(t)
 
-
-        from collections import defaultdict
-        dic = defaultdict(int)
-        for char in s:
-            dic[char] += 1
-        for char in t:
-            dic[char] -= 1
-        for v in dic.values():
-            if v != 0:
-                return False
-        return True
+        for sindex in s:
+            if sindex in hash_table1:
+                hash_table1[sindex]+=1
+            else:
+                hash_table1[sindex]=1
+        for tindex in t:
+            if tindex in hash_table2:
+                hash_table2[tindex]+=1
+            else:
+                hash_table2[tindex]=1
         
+        return hash_table1==hash_table2
+    
